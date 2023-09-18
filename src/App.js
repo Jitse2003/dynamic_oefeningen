7f434e21869b512e963085eaa7e4400eb6b8192b
Jitse2003 <r0887076@student.thomasmore.be>
2023-09-18 14:44:10 +0200

    Excercise 3.3

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hallo :)</h1>
-    </div>
-  );
+
+    return (
+
+        <div className="App">
+                <h1>Menu</h1>
+                <div>Cola</div>
+                <div>Water</div>
+                <div>Bier</div>
+                <div>Wijn</div>
+        </div>
+    );
 }
 
 export default App;
