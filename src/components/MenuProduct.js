42d76dfe410be3ebb048fbe4f22afbc1a5594e1d
Jitse2003 <r0887076@student.thomasmore.be>
2023-09-20 14:49:43 +0200

    Fix PropTypes

--- src/components/MenuProduct.js
+++ src/components/MenuProduct.js
@@ -1,25 +1,27 @@
 import PropTypes from 'prop-types';
 
-MenuProduct.propTypes = {name: PropTypes.string, price: PropTypes.string, note : PropTypes.string}
 export function MenuProduct(props) {
     const {product} = props;
 
     if (!product?.name) return;
 
-    return <div style={{display:"flex", marginRight:"20em"}}> <span style={{flex:"auto"}}> {product.name} <span style={{fontSize:"smaller", color: "blue"}}><ProductSize product = {product}></ProductSize> <ProductNote product = {product}/> </span> </span>  {product.price}€</div>;
+    return <div style={{display: "flex", marginRight: "20em"}}><span style={{flex: "auto"}}> {product.name} <span
+        style={{fontSize: "smaller", color: "blue"}}><ProductSize product={product}></ProductSize> <ProductNote
+        product={product}/> </span> </span> {product.price}€</div>;
 }
 
-function ProductSize(props){
+MenuProduct.propTypes = {props: PropTypes.shape({name:PropTypes.string, price:PropTypes.number, note:PropTypes.string})}
+
+function ProductSize(props) {
     const {product} = props;
 
     return product.size ? "(" + product.size + "CL)" : "";
 }
 
-function ProductNote(props){
+function ProductNote(props) {
     const {product} = props;
 
     return <div>{product.note ? product.note : ""}</div>
-
 
 
 }
